89c9931c59bb58bf7e698c966857a082110ccfa0
Sugar310-system <76143173+Sugar310-system@users.noreply.github.com>
2021-12-13 16:41:42 +0900

    Update myled.c

--- myled.c
+++ myled.c
@@ -48,25 +48,25 @@ static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_
 			gpio_base[7] = 1 << l;
 			__delay(10000000);
 			gpio_base[7] = 1 << l2;
-			__delay(10000000)
+			__delay(10000000);
 			gpio_base[7] = 1 << l3;
 			__delay(10000000);
 			gpio_base[7] = 1 << l4;
 			__delay(10000000);
 			gpio_base[7] = 1 << l5;
-			__delay(10000000)
+			__delay(10000000);
 			gpio_base[7] = 1 << l6;
 			__delay(10000000);
 			gpio_base[10] = 1 << l;
 			__delay(10000000);
 			gpio_base[10] = 1 << l2;
-			__delay(10000000)
+			__delay(10000000);
 			gpio_base[10] = 1 << l3;
 			__delay(10000000);
 			gpio_base[10] = 1 << l4;
 			__delay(10000000);
 			gpio_base[10] = 1 << l5;
-			__delay(10000000)
+			__delay(10000000);
 			gpio_base[10] = 1 << l6;
 			__delay(10000000);
 		}
